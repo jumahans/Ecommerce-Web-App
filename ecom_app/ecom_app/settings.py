2cdb7865b0919fbd4b60301846ec39cb701190e1
jumahans <hjmuthoni@gmail.com>
2025-12-28 13:23:52 +0300

    removed dotenv

--- ecom_app/ecom_app/settings.py
+++ ecom_app/ecom_app/settings.py
@@ -15,8 +15,6 @@ import os
 import dj_database_url
 from decouple import config
 
-from dotenv import load_dotenv
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
